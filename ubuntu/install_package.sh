4bb80f676b4a1bc2411ab91f570e7b271e9ef794
ashley <nguyenthanhcong101096@gmail.com>
2021-06-17 20:00:18 +0700

    master

--- ubuntu/install_package.sh
+++ ubuntu/install_package.sh
@@ -25,6 +25,17 @@ apt-get install containerd -y
 mkdir -p /etc/containerd
 containerd config default  /etc/containerd/config.toml
 
+#install docker
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt-get update
+apt-get install docker-ce -y
+sudo usermod -aG docker app
+su - app
+id -nG
+
 #install kubectl
 apt-get update &&  apt-get install -y apt-transport-https gnupg2 curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
